6e142c2fa77bcb1b8516528e5f46068421f9026d
rifanid98 <adninsijawa.office@gmail.com>
2021-06-19 12:56:42 +0700

    refactor folder struture

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,9 +1,12 @@
 package app
 
 import (
-	controller "bookstore_users-api/controllers"
+	ping "bookstore_users-api/controllers/ping"
+	users "bookstore_users-api/controllers/users"
 )
 
 func mapUrls() {
-	router.GET("/ping", controller.Ping)
+	router.GET("/ping", ping.Ping)
+	router.GET("users/:user_id", users.GetUser)
+	router.POST("users/", users.CreateUser)
 }
---
+++ controllers/users/users_controller.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "GetUser Not yet implemented")
+}
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "CreateUser Not yet implemented")
+}
